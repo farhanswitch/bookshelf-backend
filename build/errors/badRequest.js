99dfcef6ecfe3829eca5b600c1a9ebd90b414e13
farhanswitch <muhammad_farhan@outlook.co.id>
2022-09-08 06:04:02 -0700

    delete folder build

--- build/errors/badRequest.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BadRequestError extends Error {
-    constructor(message, name = "Bad Request") {
-        super(message);
-        this.name = name;
-    }
-}
-exports.default = BadRequestError;
